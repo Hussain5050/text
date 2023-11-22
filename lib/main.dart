5bdf883d13df3015fa3fba9c8d99fc0885218dcb
Hussain5050 <151621737+Hussain5050@users.noreply.github.com>
2023-11-22 02:32:54 -0800

    Initiat commit
    This is just plain flutter starting project.

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,11 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Container(
+              height: 60,
+              width: 50,
+              color: Colors.red,
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
